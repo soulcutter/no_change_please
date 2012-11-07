f9b3ee972e674adc410355d7f27904bbc33fce92
Bradley Schaefer <bradley.schaefer@gmail.com>
2012-11-07 08:00:46 -0600

    Removed unused solve method in NoChangePlease module

--- lib/no_change_please.rb
+++ lib/no_change_please.rb
@@ -19,24 +19,4 @@ module NoChangePlease
 
     Problem.new(target, menu)
   end
-
-  def solve(target_price, menu)
-    menu = menu.dup.sort! { |x, y| y.price <=> x.price }
-
-    return [] if menu.empty?
-
-    exact_match = menu.find { |x| x.price == target_price }
-    return [exact_match] if exact_match
-
-    # eliminate menu items more expensive than the target price
-    menu.select! { |x| target_price > x.price }
-
-    # subtract each menu item from the target price and try to solving the resulting target price recursively
-    solutions = menu.map { |menu_item| [menu_item] + solve(target_price - menu_item.price, menu) }
-
-    # get rid of solutions that do not equal our target price
-    solutions.reject! { |solution| solution.inject(0) { |sum, menu_item| sum + menu_item.price } != target_price }
-
-    solutions.empty? ? solutions : solutions.first
-  end
 end
